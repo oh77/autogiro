f204c36bfbbde90f1a94057824dfa36692047f2e
Andreas <andreas@jagelund.se>
2019-04-04 15:25:28 +0200

    Remove dist folder

--- dist/generator/Converter.d.ts
+++
@@ -1,8 +0,0 @@
-import { Payment } from '../models/Payment';
-export declare class Converter {
-    private _line;
-    constructor(inLine: string);
-    convert(): Payment;
-    private getNumValue;
-    private getAlphaValue;
-}
