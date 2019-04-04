0532c682a369c36e85aa4c0dcf89a4379b064e4b
Andreas Jagelund <andreas@jagelund.se>
2019-04-04 15:27:08 +0200

    Feature/remove demo code (#5)
    * Remove initial demo code
    
    * Remove dist folder

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function hello(str) {
-    return `Hello ${str}`;
-}
-exports.hello = hello;
--- dist/models/Fields.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Field {
-    position: number;
-    length: number;
-    isLeft: boolean;
-    padding: string;
-    type: string;
-    constructor(position: number, length: number, type: string);
-}
